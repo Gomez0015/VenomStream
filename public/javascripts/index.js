c1f3d11914b1dbf1a7c4d3041a55df99f85633ae
Gomez0015 <oscargomez.raxo@gmail.com>
2021-08-05 18:03:08 +0100

    Soda Player Install Button

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -22,9 +22,6 @@ var modal = document.getElementById("myModal");
 // Get the button that opens the modal
 var btn = document.getElementById("addMovieIcon");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on the button, open the modal
 if (btn != undefined) {
     btn.onclick = function() {
@@ -32,11 +29,7 @@ if (btn != undefined) {
     }
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-        modal.style.display = "none";
-    }
-    // When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
@@ -94,6 +87,10 @@ document.getElementById("addMovieSubmit").addEventListener("click", function(e) 
 
 function playMovie(index) {
     document.getElementById("myModal" + index).style.display = "block";
+}
+
+function closeMoviePopup(index) {
+    document.getElementById('myModal' + index).style.display = 'none';
 }
 
 function playSeries(title, element) {
